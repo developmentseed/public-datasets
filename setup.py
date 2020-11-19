3c5ed94dc95a795526c324b7ad2fbb3f92142747
vincentsarago <vincent.sarago@gmail.com>
2020-11-19 11:15:55 -0500

    fix namepaces

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 """Setup."""
 
+from typing import List
+
 from setuptools import find_namespace_packages, setup
 
 # Runtime requirements.
-inst_reqs = ["rasterio", "stac-pydantic"]
+inst_reqs: List[str] = []
 
 extra_reqs = {
-    "test": ["pytest", "pytest-cov", "pytest-asyncio", "requests"],
     "docs": ["nbconvert", "mkdocs", "mkdocs-material", "mkdocs-jupyter", "pygments"],
 }
 
@@ -15,7 +16,9 @@ setup(
     name="public-datasets",
     version="0.1.0",
     python_requires=">=3.7",
-    packages=find_namespace_packages(include=["public_datasets.*"]),
+    packages=find_namespace_packages(
+        include=["public_datasets.*"], exclude=["*tests*"],
+    ),
     install_requires=inst_reqs,
     extras_require=extra_reqs,
 )
